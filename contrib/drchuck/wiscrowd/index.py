49bd3308444f3a2138dd314284d3e5fa35730045
drchuck <drchuck@09013fc6-df45-11dd-a550-c997509ed985>
2009-01-12 00:29:20 +0000

    Clean up the flow

--- contrib/drchuck/wiscrowd/index.py
+++ contrib/drchuck/wiscrowd/index.py
@@ -11,6 +11,14 @@ from util.sessions import Session
 import dotest
 from imsglobal.lti import LTI
 
+class LogoutHandler(webapp.RequestHandler):
+
+  def get(self):
+    self.session = Session()
+    self.session.delete('lti_launch_key')
+    self.session.delete('lti_launch_password')
+    self.redirect("/")
+
 class MainHandler(webapp.RequestHandler):
 
   def prt(self,outstr):
@@ -29,12 +37,18 @@ class MainHandler(webapp.RequestHandler):
     self.prtln("<pre>")
       
     if ( not lti.launch ) :
-      self.prtln("LTI Runtime failed to start")
-      self.prtln("To simulate an LMS launch go <a href=/dotest>here</a>")
+      self.prtln("LTI Runtime not started")
+      self.prtln("")
+      self.prtln("This tool must be launched from a ")
+      self.prtln("Learning Management System (LMS) using the Simple LTI")
+      self.prtln("launch protocol.")
+      self.prtln("")
+      self.prtln("To simulate an LMS launch go <a href=/lms>here</a>")
       self.prtln("</pre>")
       return
 
     self.prtln("LTI Runtime started")
+    self.prtln("<a href=/logout>Logout</a>")
     self.prtln("User:"+lti.getUserName())
     self.prtln("Course:"+lti.getCourseName())
     if ( lti.isInstructor() ) :
@@ -47,7 +61,8 @@ class MainHandler(webapp.RequestHandler):
 
 def main():
   application = webapp.WSGIApplication([
-     ('/dotest', dotest.DoTest),
+     ('/lms', dotest.DoTest),
+     ('/logout', LogoutHandler),
      ('/.*', MainHandler)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
