78980f029568ff343f2cb512470f1557b8d4fa08
drchuck <drchuck@09013fc6-df45-11dd-a550-c997509ed985>
2009-01-16 15:12:36 +0000

    Make the portal even better!

--- trunk/contrib/drchuck/wiscrowd/wiscrowd/wiscrowd.py
+++ trunk/contrib/drchuck/wiscrowd/wiscrowd/wiscrowd.py
@@ -23,7 +23,9 @@ class ToolRegistration():
 
 # Return out Registration
 def wiscrowd():
-   return ToolRegistration('/wiz', WisHandler, "Wisdom of Crowds", "This application allows you to play games")
+   return ToolRegistration('/wiz', WisHandler, "Wisdom of Crowds", """This application allows you to play games
+where you are trying to determine something as a group by averaging many independent guesses.  
+It is basesd on the book by James Suroweki called "The Wisdom of Crowds""")
 
 class WisHandler(webapp.RequestHandler):
 
