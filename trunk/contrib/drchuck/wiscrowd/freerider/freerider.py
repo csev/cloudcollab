8572505fe31e97cbf366307d14ab5a8a5ec91e92
drchuck <drchuck@09013fc6-df45-11dd-a550-c997509ed985>
2009-01-20 21:50:38 +0000

    FreeRider coming along

--- trunk/contrib/drchuck/wiscrowd/freerider/freerider.py
+++ trunk/contrib/drchuck/wiscrowd/freerider/freerider.py
@@ -9,6 +9,8 @@ from google.appengine.api import memcache
 from util.sessions import Session
 from imsglobal.lti import LTI
 
+playercount = 2
+
 # Move this to LMS
 class ToolRegistration():
 
@@ -52,8 +54,25 @@ class FreeRiderHandler(webapp.RequestHandler):
     freekey = "FreeRider-"+str(lti.course.key())
     logging.info("Loading Free key="+freekey)
     freerider =  memcache.get(freekey)
+    newride = False
     if freerider == None:
       freerider = dict()
+      newride = True
+
+    if not 'players' in freerider:
+       freerider["players"] = list()
+    if not 'chips' in freerider:
+       freerider["chips"] = list()
+    if not 'last' in freerider:
+       freerider["last"] = list()
+    if not 'turn' in freerider:
+       freerider['turn'] = 0
+    if not 'current' in freerider:
+       freerider['current'] = 0
+    if not 'pot' in freerider:
+       freerider['pot'] = 0
+
+    if newride : 
       memcache.add(freekey, freerider, 3600)
     return freerider
 
@@ -70,20 +89,23 @@ class FreeRiderHandler(webapp.RequestHandler):
     istr = self.markup()
     if ( istr != None ) : self.response.out.write(istr)
 
-  def mainscreen(self, lti):
+  # All your base are belong to us!
+  def mainscreen(self, lti, vars = { }):
     rendervars = {'username': lti.user.email, 
                   'course': lti.getCourseName(), 
                   'messagesaction' : getactionpath(self,"messages", True), 
-                  'playaction' : getactionpath(self,"play", True), 
+                  'playaction' : getactionpath(self,"play"), 
                   'request': self.request}
+    rendervars.update(vars)
 
     if lti.isInstructor() : 
-      rendervars['resetaction'] = getactionpath(self,"reset", True)
+      rendervars['resetaction'] = getactionpath(self,"reset")
       rendervars['instructor'] = "yes"
 
     data = self.getmodel(lti)
-    if ( len(data) < 4 ) :
-      rendervars['joinaction'] = getactionpath(self,"join", True)
+    players = data["players"]
+    if ( len(players) < 4 ) :
+      rendervars['joinaction'] = getactionpath(self,"join")
 
     temp = os.path.join(os.path.dirname(__file__), 'templates/index.htm')
     outstr = template.render(temp, rendervars)
@@ -105,67 +127,140 @@ class FreeRiderHandler(webapp.RequestHandler):
     action = getaction(self)
     if action == "messages" : return self.action_messages(lti)
     if action == "join" : return self.action_join(lti)
+    if action == "reset" : return self.action_reset(lti)
     if action == "play" : return self.action_play(lti)
 
     return self.mainscreen(lti)
 
+  def action_reset(self,lti) :
+    if not lti.isInstructor() :
+      msg = "Only the instructor can reset!"
+      return self.mainscreen(lti, {'msg' : msg})
+    data = dict()
+    self.putmodel(data, lti)
+    msg = "Game data reset"
+    return self.mainscreen(lti, {'msg' : msg})
+
   def action_join(self,lti) :
     data = self.getmodel(lti)
+    players = data["players"]
+    chips = data["chips"]
+    last = data["last"]
+    if len(players) >= playercount: 
+      msg = "The game is closed - you can play next time"
+      return self.mainscreen(lti, {'msg' : msg})
+
+    if not lti.user.email or len(lti.user.email) < 1:
+      msg = "You must have an E-Mail to play"
+      return self.mainscreen(lti, {'msg' : msg})
     
+    if lti.user.email in players:
+      msg = "You have already joined the game"
+      return self.mainscreen(lti, {'msg' : msg})
+
+    players.append(lti.user.email)
+    chips.append(20)
+    last.append( list () )
+    data["players"] = players
+    data["chips"] = chips
+    data["last"] = last
+
+    # Let the game begin!
+    if len(players) >= playercount: 
+       data['turn'] = 1
+       data['current'] = 0
+       
+    self.putmodel(data, lti)
+
+    msg = "Welcome to the game"
+    return self.mainscreen(lti, {'msg' : msg})
+
+  def action_play(self,lti) :
+    data = self.getmodel(lti)
+    players = data["players"]
+    chips = data["chips"]
+    last = data["last"]
+    current = data["current"]
+    turn = data["turn"]
+    pot = data["pot"]
+
+    if turn < 1 or turn > 4: 
+      msg = "The game is not running!"
+      return self.mainscreen(lti, {'msg' : msg})
+    if not lti.user.email in players:
+      msg = "You are not currently playing!"
+      return self.mainscreen(lti, {'msg' : msg})
+    if not lti.user.email == players[current] :
+      msg = "It is not your turn!"
+      return self.mainscreen(lti, {'msg' : msg})
+
+    contrib = self.request.get('chips')
+    try: contrib = int(contrib)
+    except: contrib = -1
+    
+    if contrib > chips[current] : contrib = chips[current]
+
+    if contrib < 0 :
+      msg = "Please enter a real number"
+      return self.mainscreen(lti, {'msg' : msg})
+
+    pot = pot + contrib
+    chips[current] = chips[current] - contrib
+    last[current].append(contrib)
+    current = current + 1
+    if current >= playercount:
+      current = 0
+      turn = turn + 1
+      # Split the pot
+      each = ((pot * 1.6) / playercount) 
+      for i in range(len(chips)):
+        chips[i] = chips[i] + each
+      pot = 0
+
+    data["pot"] = pot
+    data["current"] = current
+    data["turn"] = turn
+    data["chips"] = chips
+    data["last"] = last
+
+    self.putmodel(data, lti)
+
+    msg = "Thanks for your contribution!"
+    return self.mainscreen(lti, {'msg' : msg})
 
   def action_messages(self,lti) :
     data = self.getmodel(lti)
      
-    return "Player count: "+str(len(data))
+    players = data["players"]
+    chips = data["chips"]
+    turn = data["turn"]
+    current = data["current"]
+    last = data["last"]
+    pot = data["pot"]
 
-    name = self.request.get("name")
-    if len(name) < 1 : name = lti.user.email
+    r = "<pre>\n"
 
-    guess = self.request.get("guess")
+    if turn > 0 and turn < 5 and lti.user.email == players[current]:
+      r = r + '<font color="red">It is your turn</font>\n\n'
 
-    try: guess = int(guess)
-    except: guess = -1
-
-    msg = ""
-    if lti.isInstructor() and string.lower(name) == "reset":
-       data = dict()
-       freerider.blob = pickle.dumps( data ) 
-       freerider.put()
-       msg = "Data reset"
-    elif guess < 1 :
-       msg = "Please enter a valid, numeric guess"
-    elif  len(name) < 1 : 
-       msg = "No Name Found"
-    elif name in data : 
-       msg = "You already have answered this"
+    if turn < 1:
+      r = r + "Game has not started\n"
+    elif turn > 4:
+      r = r + "Game completed\n"
     else:
-       ret = db.run_in_transaction(self.addname, freerider.key(), name, guess)
-       if ret : 
-         data = ret
-         msg = "Thank you for your guess"
-       else:
-         msg = "Unable to store your guess please re-submit"
+      r = r + "Current turn: "+str(turn)+"\n";
 
-    rendervars = {'username': lti.user.email, 
-                  'course': lti.getCourseName(), 
-                  # 'msg' : msg, 
-                  'request': self.request}
-    if lti.isInstructor() : rendervars['instructor'] = "yes"
+    r = r + "\n"
+    if lti.user.email in players and not lti.isInstructor() :
+      r = r + "Players: "+str(len(players))+"\n"
+    else:
+      r = r + "Current pot total: " + str(pot) + "\n"
+      for i in range(len(players)):
+         if current == i :
+           r = r + "===> "
+         else:
+           r = r + "     "
+         r = r + "(" + str(chips[i]) + " / " + str(last[i]) + ") " + players[i] + "\n"
 
-    if lti.isInstructor() and len(data) > 0 :
-       text = ""
-       total = 0
-       for (key, val) in data.items(): 
-         text = text + key + "," + str(val) + "\n"
-         total = total + val
-       count = len(data)
-       ave = 0
-       if count > 0 : ave = total / count
-       rendervars["ave"] = ave
-       rendervars["count"] = count
-       rendervars["data"] = text
-
-    temp = os.path.join(os.path.dirname(__file__), 'templates/index.htm')
-    outstr = template.render(temp, rendervars)
-    return outstr
-
+    r = r + "</pre>\n"
+    return r
