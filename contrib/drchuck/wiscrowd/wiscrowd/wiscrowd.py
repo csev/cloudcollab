0417c0236a3be23aae0a924a7c96dfefd9105094
drchuck <drchuck@09013fc6-df45-11dd-a550-c997509ed985>
2009-02-03 21:20:50 +0000

    Begin to work through the "module" pattern for tool registration.

--- contrib/drchuck/wiscrowd/wiscrowd/wiscrowd.py
+++ contrib/drchuck/wiscrowd/wiscrowd/wiscrowd.py
@@ -8,17 +8,10 @@ from google.appengine.ext import db
 
 from util.sessions import Session
 from imsglobal.lti import LTI
+from core.tool import ToolRegistration
 
 class Wisdom(db.Model) :
    blob = db.BlobProperty()
-
-class ToolRegistration():
-
-  def __init__(self,path,handler,title,desc):
-    self.path = path
-    self.handler = handler
-    self.title = title
-    self.desc = desc
 
 # Return out Registration
 def wiscrowd():
