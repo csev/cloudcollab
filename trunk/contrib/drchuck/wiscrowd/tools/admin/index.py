ae32632bc3049417cdfe5e07ad3b45ec372a6f03
drchuck <drchuck@09013fc6-df45-11dd-a550-c997509ed985>
2009-02-14 04:02:38 +0000

    Working on a real login

---
+++ trunk/contrib/drchuck/wiscrowd/tools/admin/index.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import pickle
+import wsgiref.handlers
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+from google.appengine.ext import db
+
+from util.sessions import Session
+from imsglobal.lti import Context
+from core.tool import ToolRegistration
+
+# Return our Registration
+def register():
+   return ToolRegistration(AdminHandler, "Administration Tool", 
+     """This application allows you to administer this system.""")
+
+class AdminHandler(webapp.RequestHandler):
+
+  outStr = ""
+
+  def get(self):
+    istr = self.markup()
+    if ( istr != None ) : self.response.out.write(istr)
+
+  def post(self):
+    istr = self.markup()
+    if ( istr != None ) : self.response.out.write(istr)
+
+  # This method returns tool output as a string
+  def markup(self):
+    self.session = Session()
+    lti = Context(self, self.session);
+
+    if ( lti.complete ) : return
+
+    if lti.isAdmin() : return "HELLO MASTER"
+    else: return "YOU ARE NOT ADMIN"
