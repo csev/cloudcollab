e11486718e57e6169e0a40e1a63699166c96b929
drchuck <drchuck@09013fc6-df45-11dd-a550-c997509ed985>
2009-03-10 19:58:05 +0000

    Switch the admin tool to be a portlet.

--- contrib/drchuck/wiscrowd/tools/admin/index.py
+++ contrib/drchuck/wiscrowd/tools/admin/index.py
@@ -1,38 +1,13 @@
-import logging
-import os
-import pickle
-import wsgiref.handlers
-from google.appengine.ext.webapp import template
-from google.appengine.ext import webapp
-from google.appengine.ext import db
-
-from util.sessions import Session
 from core.tool import ToolRegistration
-from imsglobal.lticontext import LTI_Context
+from core import learningportlet
 
 # Return our Registration
 def register():
    return ToolRegistration(AdminHandler, "Administration Tool", 
      """This application allows you to administer this system.""")
 
-class AdminHandler(webapp.RequestHandler):
+class AdminHandler(learningportlet.LearningPortlet):
 
-  outStr = ""
-
-  def get(self):
-    istr = self.markup()
-    if ( istr != None ) : self.response.out.write(istr)
-
-  def post(self):
-    istr = self.markup()
-    if ( istr != None ) : self.response.out.write(istr)
-
-  # This method returns tool output as a string
-  def markup(self):
-    self.session = Session()
-    lti = LTI_Context(self, self.session);
-
-    if ( lti.complete ) : return
-
-    if lti.isAdmin() : return "HELLO MASTER"
+  def getview(self, info):
+    if self.context.isAdmin() : return "HELLO MASTER"
     else: return "YOU ARE NOT ADMIN"
