f0bfa61dfed8bd966d8d4aad639cc97fe50510b6
drchuck <drchuck@09013fc6-df45-11dd-a550-c997509ed985>
2009-03-19 12:51:20 +0000

    Fix copyright.

--- contrib/drchuck/java-framework/simplelti/WEB-INF/classes/org/imsglobal/simplelti/XMLMap.java
+++ contrib/drchuck/java-framework/simplelti/WEB-INF/classes/org/imsglobal/simplelti/XMLMap.java
@@ -3,7 +3,7 @@
  * $Id$
  **********************************************************************************
  *
- * Copyright (c) 2008 Etudes, Inc.
+ * Copyright (c) 2009 IMS Global Learning Consortium, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
